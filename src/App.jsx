149af9bdb9ff87190b735d274c6e2a411c476812
SayaniSen2022 <sayanisen.official@gmail.com>
2023-07-11 16:44:53 +0530

    checking bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Hero from "./components/Hero";
 import Error from "./components/Error";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Blogs from "./components/Blogs";
 
 function App() {
   return (
