933336d9dcab2f2be662474343afa9a073f96f5f
SayaniSen2022 <sayanisen.official@gmail.com>
2023-09-03 21:21:07 +0530

    hero res update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,29 +12,63 @@ const Hero = () => {
     workRef.current?.scrollIntoView({ behavior: "smooth" });
   };
   return (
-    <div className="hero-section">
-      <Stack>
-        <HStack>
-          <Box maxW="35rem">
-            <Heading as="h1" size="4xl" my="10rem" ml="5rem">
+    <>
+      <div className="hero-section">
+        <Stack>
+          <HStack>
+            <Box maxW="35rem">
+              <Heading
+                as="h1"
+                size="4xl"
+                my="10rem"
+                ml="5rem"
+                lineHeight="initial"
+              >
+                Get ideas turned into <Text className="typed">reality.</Text>
+              </Heading>
+            </Box>
+            <Box maxW="45rem">
+              <Heading as="h1" size="2xl" my="10rem" ml="5rem">
+                I'm Sayani. A front-end developer.
+              </Heading>
+            </Box>
+          </HStack>
+          <div id="explore-button">
+            <IconContext.Provider value={{ className: "arrow", size: 70 }}>
+              <MdKeyboardDoubleArrowDown onClick={handleClick} />
+            </IconContext.Provider>
+          </div>
+        </Stack>
+
+        <Projects ref={workRef} />
+      </div>
+      <div className="hero-mobile">
+        <Stack>
+          <Box maxW="25rem">
+            <Heading
+              as="h1"
+              size="4xl"
+              my="3rem"
+              ml="3rem"
+              lineHeight="initial"
+            >
               Get ideas turned into <Text className="typed">reality.</Text>
             </Heading>
           </Box>
-          <Box maxW="45rem">
-            <Heading as="h1" size="2xl" my="10rem" ml="5rem">
+          <Box maxW="20rem">
+            <Heading as="h1" size="3xl" ml="3rem">
               I'm Sayani. A front-end developer.
             </Heading>
           </Box>
-        </HStack>
-        <div id="explore-button">
-          <IconContext.Provider value={{ className: "arrow", size: 70 }}>
-            <MdKeyboardDoubleArrowDown onClick={handleClick} />
-          </IconContext.Provider>
-        </div>
-      </Stack>
-
-      <Projects ref={workRef} />
-    </div>
+          <div id="explore-button">
+            <IconContext.Provider value={{ className: "arrow", size: 70 }}>
+              <MdKeyboardDoubleArrowDown onClick={handleClick} />
+            </IconContext.Provider>
+          </div>
+        </Stack>
+        <Projects ref={workRef} />
+      </div>
+    </>
   );
 };
 export default Hero;
